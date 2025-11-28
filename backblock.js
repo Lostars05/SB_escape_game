37c2a3aa6aaf8d5146f8a4ea84bb8fb17483ac72
Lostars05 <chaey0213@gmail.com>
2025-11-29 04:07:58 +0900

    Update backblock.js
    escape_this_page.html > index.html

--- backblock.js
+++ backblock.js
@@ -2,6 +2,6 @@
 window.addEventListener("DOMContentLoaded", function () {
   history.pushState(null, null, location.href);
   window.addEventListener("popstate", function () {
-    location.href = "escape_this_page.html";
+    location.href = "index.html";
   });
 });
